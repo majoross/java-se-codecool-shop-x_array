fa39bd7a375e8595f119d91221c2ee1bfd7a0a58
Anna Racz <nnaracz@gmail.com>
2017-04-25 14:41:54 +0200

    shopping cart initial setup

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -2,9 +2,11 @@ package com.codecool.shop.controller;
 
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.ShoppingCartDao;
 import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.dao.implementation.ShoppingCartDaoMem;
 import com.codecool.shop.dao.implementation.SupplierDaoMem;
 import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
@@ -22,12 +24,14 @@ public class ProductController {
     public static ModelAndView renderProducts(Request req, Response res) {
         ProductDao productDataStore = ProductDaoMem.getInstance();
         ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+        ShoppingCartDao shoppingCartDataStore = ShoppingCartDaoMem.getInstance();
 
         Map params = new HashMap<>();
         params.put("category", productCategoryDataStore.find(1));
-        System.out.println("THIS IS SOMETHING");
+        //System.out.println("THIS IS SOMETHING");
         params.put("products", productDataStore.getBy(productCategoryDataStore.find(1)));
-        System.out.println("THIS IS ANOTHER THING");
+        //System.out.println("THIS IS ANOTHER THING");
+        params.put("cart", shoppingCartDataStore.getAll());
         return new ModelAndView(params, "product/index");
     }
 
---
+++ src/main/java/com/codecool/shop/dao/implementation/ShoppingCartDaoMem.java
@@ -0,0 +1,57 @@
+package com.codecool.shop.dao.implementation;
+
+import com.codecool.shop.dao.ShoppingCartDao;
+import com.codecool.shop.model.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ShoppingCartDaoMem implements ShoppingCartDao {
+
+    //List to store items in the cart??
+    private List<Product> DATA = new ArrayList<>();
+
+    //initially there are no shopping cart instances
+    private static ShoppingCartDaoMem instance = null;
+
+    //private constructor
+    private ShoppingCartDaoMem(){}
+
+    //if there are no instances of cart, creates one
+    public static ShoppingCartDaoMem getInstance() {
+        if (instance == null) {
+            instance = new ShoppingCartDaoMem();
+            System.out.println("shopping cart instance created");
+        }
+        return instance;
+    }
+
+    @Override
+    public void add(Product product) {
+        product.setId(DATA.size() + 1);
+        DATA.add(product);
+    }// add item to cart, which is the DATA list of products
+
+    @Override
+    public void remove(int id) {
+        DATA.remove(find(id));
+    }
+
+    public void review(int id) { /*redirect to Shopping cart main page*/ }
+
+    @Override
+    public Product find(int id) {
+        return DATA.stream().filter(t -> t.getId() == id).findFirst().orElse(null);
+    }
+
+    @Override
+    public List<Product> getAll() {
+        return DATA;
+    }
+
+
+
+
+    }
+
